6e51d5d5871dbfbd2652fb838252124d1d1cecf0
Dennis <dang.dennis21@gmail.com>
2020-03-30 22:56:43 -0700

    finished creating preprocessing file, working on model

--- dataset/create_json.py
+++ dataset/create_json.py
@@ -5,102 +5,101 @@ cnn_path = "cnn//"
 output_json_path = "output.json"
 allowed_characters = "abcdefghijklmnopqrstuvwxyz0123456789 "
 
-limit = {
-	'max_text' : 4
-	'max_summary' : 8
-}
+############
+#   Main   #
+############
+def main(filepath):
+    turn_to_json(filepath)
 
 ####################################
 #   Creates JSON files from data   #
 ####################################
-
 def turn_to_json(filepath):
-	files = [cnn_path + f for f in os.listdir(filepath)]
-	json_data = list()
-	counter = 0
+    files = [cnn_path + f for f in os.listdir(filepath)]
+    json_data = list()
+    counter = 0
 
-	for file in files:
-		if counter%10000 == 0:
-			print("Progress: {}/{} files".format(counter, len(files)))
-		counter += 1
+    for file in files:
+        if counter%10000 == 0:
+            print("Progress: {}/{} files".format(counter, len(files)))
+        counter += 1
 
-		data = dict()
+        data = dict()
 
-		try:
-			text, summary = get_text_summary(file)
-		except:
-			continue
+        try:
+            text, summary = get_text_summary(file)
+        except:
+            continue
 
-		data['text'] = text
-		data['summary'] = summary
+        data['text'] = text
+        data['summary'] = summary
 
-		json_data.append(data)
+        json_data.append(data)
 
-	# write json to file
-	with open(output_json_path, 'w') as outputFile:
-		json.dump(json_data, outputFile, indent=2)
+    # write json to file
+    with open(output_json_path, 'w') as outputFile:
+        json.dump(json_data, outputFile, indent=2)
 
 
 
-#############################################################
-#   Extract text and corresponding summary from data file   #
-#############################################################
-
+#####################################################################
+#   Helper: Extract text and corresponding summary from data file   #
+#####################################################################
 def get_text_summary(filePath):
-	file_text = list()
-	file_summary = list()
-	
-	try:
-		with open(filePath, 'r') as file:
-			lines = file.readlines()
-	except:
-		raise Exception()
+    file_text = list()
+    file_summary = list()
+    
+    try:
+        with open(filePath, 'r') as file:
+            lines = file.readlines()
+    except:
+        raise Exception()
 
-	
-	highlight_start = 0
-	lines = [line.replace("\n", '') for line in lines]
+    
+    highlight_start = 0
+    lines = [line.replace("\n", '') for line in lines]
+    lines = [line.replace("cnn", '') for line in lines]
 
-	for (i, line) in enumerate(lines):
-		if line != '':
-			if line != "@highlight":
-				file_text.append(filter_chars(line))
-			else:
-				highlight_start = i
-				break;
+    for (i, line) in enumerate(lines):
+        if line != '':
+            if line != "@highlight":
+                file_text.append(filter_chars(line))
+            else:
+                highlight_start = i
+                break;
 
-	for line in lines[highlight_start:]:
-		if line != '' and line != "@highlight":
-			file_summary.append(filter_chars(line))
-
-
-	text = " ".join(file_text)
-	summary = " ".join(file_summary)
-
-	return text, summary
+    for line in lines[highlight_start:]:
+        if line != '' and line != "@highlight":
+            file_summary.append(filter_chars(line))
 
 
+    text = " ".join(file_text)
+    summary = " ".join(file_summary)
 
-###############################################################################
-#   Converts to lower case, removes extraneous characters and double spaces   #
-###############################################################################
+    return text, summary
 
+
+
+#######################################################################################
+#   Helper: Converts to lower case, removes extraneous characters and double spaces   #
+#######################################################################################
 def filter_chars(line):
-	filtered = []
-	space = False	# used to remove double spaces
-	for char in line:
-		char = char.lower()
-		if char in allowed_characters:
-			if char == ' ':
-				if not space:
-					space = True
-					filtered.append(char)
-				else:
-					space = False
-			else:
-				filtered.append(char)
-				space = False
+    filtered = []
+    space = False   # used to remove double spaces
+    for char in line:
+        char = char.lower()
+        if char in allowed_characters:
+            if char == ' ':
+                if not space:
+                    space = True
+                    filtered.append(char)
+                else:
+                    space = False
+            else:
+                filtered.append(char)
+                space = False
 
-	return ''.join(filtered)
+    return ''.join(filtered)
 
 if __name__ == "__main__":
-	turn_to_json(cnn_path)
+    turn_to_json(cnn_path)
--- dataset/load_data.py
+++ dataset/load_data.py
@@ -1,24 +1,135 @@
+import os
 import json
+from create_json import output_json_path, cnn_path 
 
-output_json_path = "output.json"
+limits = {
+    'max_text' : 500,           # limit for max length of text
+    'num_entries' : 50000,     # limit for max num of entries
+    'vocab_size' : 10000,
+}
 
-def filter_len(json_path, max_text):
-	text = list()
-	summary = list()
+###########################
+#   Return index tables   #
+###########################
+def get_index_tables():
+    freq_table = create_frequency_table()
+    word2index = create_word2index(freq_table)
+    index2word = create_index2word(freq_table)
 
-	with open(json_path) as file:
-		data = json.load(file)
+    return (word2index, index2word)
 
-	for entry in data[:2]:
-		text_len = len(entry['text'].split(' '))
-		summary_len = len(entry['summary'].split(' '))
+#########################################################
+#   Helper: return list of ints corresponding to data   #
+#########################################################
+def index_words(data, word2index):
+    indexed_data = list()
 
-		# check to make sure that summary length is shorter than text length
-		if text_len > summary_len:
+    for entry in data:
+        indexed_entry = list()
+        for word in entry.split(' '):
+            if word in word2index:
+                value = word2index[word]
+                indexed_entry.append(value)
+
+        indexed_data.append(indexed_entry)
+
+    return indexed_data
+
+###############################################
+#   Ensures length of text is within limits   #
+###############################################
+def filter_len():
+    text = list()
+    summary = list()
+    data = load_json_data()
+
+    for entry in data:
+        text_len = len(entry['text'].split(' '))
+        summary_len = len(entry['summary'].split(' '))
+
+        # check to make sure that summary length is shorter than text length
+        if text_len > summary_len and text_len < limits['max_text']:
+            text.append(entry['text'])
+            summary.append(entry['summary'])
+
+    return (text, summary)
+
+###############################
+#   Returns frequency table   #
+###############################
+def create_frequency_table():
+    data = load_json_data()
+
+    freq_table = dict()
+
+    for entry in data:
+        text = entry['text'].split(' ')
+        summary = entry['summary'].split(' ')
+
+        count_words(freq_table, text)
+        count_words(freq_table, summary)
+
+    sorted_table = dict()
+
+    for (i,key) in enumerate(sorted(freq_table, key=freq_table.get, reverse=True)):
+        if i > limits['vocab_size']:
+          break
+        else:
+            sorted_table[key] = freq_table[key]
+
+    return sorted_table
+
+
+###############################
+# Returns word-to-index table #
+###############################
+def create_word2index(freq_table):
+    word2index = dict()
+
+    for (i, key) in enumerate(freq_table):
+        word2index[key] = i
+
+    return word2index
+
+
+#################################
+#   Returns index-to-word table #
+#################################
+def create_index2word(freq_table):
+    index2word = dict()
+
+    for (i ,key) in enumerate(freq_table):
+        index2word[i] = key
+
+    return index2word
+
+#########################################
+#   Helper: counts frequency of words   #
+#########################################
+def count_words(freq_table, words):
+    for word in words:
+        if word in freq_table:
+            freq_table[word] += 1
+        else:
+            freq_table[word] = 1
 
 
 
+#####################################
+#   Load data from JSON data file   #
+#####################################
+def load_json_data():
+    with open(output_json_path) as file:
+        data = json.load(file)
+
+    return data
 
 
-if __name__ == "__main__":
-	filter_len(output_json_path, 10)
+def find_longest_sequence(data):
+    count = -1
+
+    for entry in data:
+        if len(entry) > count:
+            count = len(entry)
+
+    return count
